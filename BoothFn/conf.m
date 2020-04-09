73b38a730ba60e855fef1f54376ea923f8abbd96
satyartpeddada <37249911+satyartpeddada@users.noreply.github.com>
2020-04-09 15:19:28 -0500

    Add files via upload

---
+++ BoothFn/conf.m
@@ -0,0 +1,6 @@
+function [n_input, lb, ub] = conf()
+    n_input = 2; % x, y
+    lb = [-10 -10];
+    ub = [10 10];
+end
+
---
+++ BoothFn/obj.m
@@ -0,0 +1,4 @@
+function f = obj(x)
+%   Booth function : f = (2x+y-5)^2 + (x+2y-7)^2
+    f = (2*x(1)+x(2)-5)^2 + (x(1)+2*x(2)-7)^2;
+end
